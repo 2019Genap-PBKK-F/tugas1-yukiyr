dc5d8acc31dfe033c75276e9ddb9075342f440e0
yukiyr <37041284+yukiyr@users.noreply.github.com>
2020-05-09 18:48:41 +0700

    Update MasterIndikator.vue

--- src/components/views/MasterIndikator.vue
+++ src/components/views/MasterIndikator.vue
@@ -38,16 +38,16 @@ export default {
   },
   methods: {
     newRow() {
-      axios.post('http://localhost:8005/api/MasterIndikator/', this.form).then(res => {
+      axios.post('http://10.199.14.46:8005/api/MasterIndikator/', this.form).then(res => {
         console.log(res.data)
       })
     },
     updateRow(instance, cell, columns, row, value) {
-      axios.get('http://localhost:8005/api/MasterIndikator/').then(res => {
+      axios.get('http://10.199.14.46:8005/api/MasterIndikator/').then(res => {
         var index = Object.values(res.data[row])
         index[columns] = value
         console.log(index)
-        axios.put('http://localhost:8005/api/MasterIndikator/' + index[0], {
+        axios.put('http://10.199.14.46:8005/api/MasterIndikator/' + index[0], {
           id: index[0],
           id_aspek: index[1],
           id_penyebut: index[2],
@@ -64,11 +64,11 @@ export default {
       })
     },
     deleteRow(instance, row) {
-      axios.get('http://localhost:8005/api/MasterIndikator/').then(res => {
+      axios.get('http://10.199.14.46:8005/api/MasterIndikator/').then(res => {
         var index = Object.values(res.data[row])
         // console.log(index)
         console.log(row)
-        axios.delete('http://localhost:8005/api/MasterIndikator/' + index[0])
+        axios.delete('http://10.199.14.46:8005/api/MasterIndikator/' + index[0])
       })
     }
   },
@@ -77,7 +77,7 @@ export default {
       return {
         data: this.MasterIndikator,
         allowToolbar: true,
-        url: 'http://localhost:8005/api/MasterIndikator/',
+        url: 'http://10.199.14.46:8005/api/MasterIndikator/',
         onchange: this.updateRow,
         oninsertrow: this.newRow,
         ondeleterow: this.deleteRow,
