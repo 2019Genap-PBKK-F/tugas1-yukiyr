128988e6d66f954a129e356f084f17342cb726ec
yukiyr <37041284+yukiyr@users.noreply.github.com>
2020-05-09 18:51:40 +0700

    Update Unit.vue

--- src/components/views/Unit.vue
+++ src/components/views/Unit.vue
@@ -31,16 +31,16 @@ export default {
   },
   methods: {
     newRow() {
-      axios.post('http://localhost:8005/api/Unit/', this.form).then(res => {
+      axios.post('http://10.199.14.46:8005/api/Unit/', this.form).then(res => {
         console.log(res.data)
       })
     },
     updateRow(instance, cell, columns, row, value) {
-      axios.get('http://localhost:8005/api/Unit/').then(res => {
+      axios.get('http://10.199.14.46:8005/api/Unit/').then(res => {
         var index = Object.values(res.data[row])
         index[columns] = value
         console.log(index)
-        axios.put('http://localhost:8005/api/Unit/' + index[0], {
+        axios.put('http://10.199.14.46:8005/api/Unit/' + index[0], {
           id: index[0],
           KategoriUnit_id: index[1],
           nama: index[2]
@@ -50,11 +50,11 @@ export default {
       })
     },
     deleteRow(instance, row) {
-      axios.get('http://localhost:8005/api/Unit/').then(res => {
+      axios.get('http://10.199.14.46:8005/api/Unit/').then(res => {
         var index = Object.values(res.data[row])
         // console.log(index)
         console.log(row)
-        axios.delete('http://localhost:8005/api/Unit/' + index[0])
+        axios.delete('http://10.199.14.46:8005/api/Unit/' + index[0])
       })
     }
   },
@@ -63,7 +63,7 @@ export default {
       return {
         data: this.KategoriUnit,
         allowToolbar: true,
-        url: 'http://localhost:8005/api/Unit/',
+        url: 'http://10.199.14.46:8005/api/Unit/',
         onchange: this.updateRow,
         oninsertrow: this.newRow,
         ondeleterow: this.deleteRow,
